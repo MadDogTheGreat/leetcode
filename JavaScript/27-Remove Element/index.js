a9c803724ab0c6443806764bb81ca7205ca6b7d2
Farhad <farhaddezhamifard@gmail.com>
2022-09-06 15:42:00 +0430

    remove element in place algo added

--- JavaScript/27-Remove Element/index.js
+++ JavaScript/27-Remove Element/index.js
@@ -18,3 +18,26 @@ var removeElement = function (nums, val) {
     return len;
 };
 
+// In-place algorithm
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElementInPlace = function (nums, val) {
+    const n = nums.length;
+
+    let writePointer = 0
+    let readPointer = 0;
+    while (readPointer < n) {
+        if (nums[readPointer] === val) {
+            readPointer++;
+        } else {
+            [nums[writePointer], nums[readPointer]] = [nums[readPointer], nums[writePointer]]
+            readPointer++;
+            writePointer++;
+        }
+    }
+
+    return writePointer;
+};
