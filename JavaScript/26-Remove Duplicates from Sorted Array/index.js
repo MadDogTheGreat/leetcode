266efba8c9df4809bf42f661782f3bf15f88a5f9
Farhad <farhaddezhamifard@gmail.com>
2022-09-06 14:40:01 +0430

    in place remove duplicate added

--- JavaScript/26-Remove Duplicates from Sorted Array/index.js
+++ JavaScript/26-Remove Duplicates from Sorted Array/index.js
@@ -2,15 +2,15 @@
  * @param {number[]} arr
  * @return {number}
  */
-var removeDuplicates = function (arr) {
+const removeDuplicates = function (arr) {
     let n = arr.length;
     if (n == 0 || n == 1)
         return n;
 
-    var temp = new Array(n);
+    let temp = new Array(n);
 
-    var j = 0;
-    for (var i = 0; i < n - 1; i++)
+    let j = 0;
+    for (let i = 0; i < n - 1; i++)
         if (arr[i] != arr[i + 1])
             temp[j++] = arr[i];
 
@@ -18,6 +18,27 @@ var removeDuplicates = function (arr) {
 
     for (var i = 0; i < j; i++)
         arr[i] = temp[i];
+
+    return j;
+};
+
+// In-place algorithm
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+const removeDuplicatesInPlace = function (arr) {
+    const n = arr.length;
+    if (n == 0 || n == 1)
+        return n;
+
+    let j = 0;
+
+    for (let i = 0; i < n - 1; i++)
+        if (arr[i] != arr[i + 1])
+            arr[j++] = arr[i];
+
+    arr[j++] = arr[n - 1];
 
     return j;
 };
